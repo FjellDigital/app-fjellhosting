ed66ec941af836d933638939d044412528b0c28f
FjellDigital <andreas@fjelldigital.no>
2025-03-10 15:36:37 +0100

    Update page-layout.js

--- src/components/page-layout.js
+++ src/components/page-layout.js
@@ -3,7 +3,6 @@ import { NavBar } from "./navigation/desktop/nav-bar";
 import { MobileNavBar } from "./navigation/mobile/mobile-nav-bar";
 import PageFooter from "./page-footer";
 
-
 export const PageLayout = ({ children }) => {
   return (
     <div className="page-layout">
